48d59a97fb2ad49041ceb727494a22a82b5e0ef0
45-Assiagnments <aaleali999@gmail.com>
2024-08-05 18:49:32 +0500

    Project Timer

---
+++ main.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import {differenceInSeconds} from "date-fns"
+
+
+const result = await inquirer.prompt([{
+    name : "userInput",
+    type : "number",
+    message: "Enter amount of Seconds:"
+}]);
+
+let input = result.userInput
+
+function startTime(val:number){
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime)
+    setInterval((()=>{
+        let currTime = new Date()
+        let timeDiff = differenceInSeconds(intervalTime, currTime)
+
+        if(timeDiff <= 0){
+            console.log(`Timer has Expired...`);   
+            process.exit()
+        }
+        const min = Math.floor((timeDiff %(3600*24))/3600)
+        const sec = Math.floor(timeDiff%60)
+        console.log(`${min.toString().padStart(2,"0")}:${sec.toString().padStart(2, "0")}`);
+        
+    }),1000) 
+}
+startTime(input);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
